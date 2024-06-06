87a56ba169d95d826ec03be982289761a75e5e6e
Juanchy Quintana <quintanajuanchy25@gmail.com>
2024-06-06 11:23:56 -0300

    se agregó zod

--- src/hooks/useWeather.ts
+++ src/hooks/useWeather.ts
@@ -1,8 +1,19 @@
 import axios from "axios";
+import { z } from "zod";
 import { SearchType } from "../types";
 
+const Weather = z.object({
+  name: z.string(),
+  main: z.object({
+    temp: z.number(),
+    temp_max: z.number(),
+    temp_min: z.number(),
+  }),
+});
+
+type Weather = z.infer<typeof Weather>;
+
 export default function useWeather() {
-    
   const fetchWeather = async (search: SearchType) => {
     const apiId = import.meta.env.VITE_API_KEY;
     try {
@@ -13,8 +24,13 @@ export default function useWeather() {
       const lon = data.coord.lon;
 
       const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiId}`;
-      const { data: weatherResult } = await axios(weatherUrl)
+      const { data: weatherResult } = await axios(weatherUrl);
 
+      const result = Weather.safeParse(weatherResult);
+      if(result.success) {
+        console.log(result.data.name)
+        console.log(result.data.main.temp)
+      }
 
     } catch (error) {
       console.log(error);
